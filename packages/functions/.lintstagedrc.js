537bde83c903a5dd44d5b43699c88e4de1aac5c9
SHØ <neila@users.noreply.github.com>
2022-12-06 17:47:59 +0900

    add git hooks

--- packages/functions/.lintstagedrc.js
+++ packages/functions/.lintstagedrc.js
@@ -1,14 +1,10 @@
 const path = require('path');
 
 const buildLintCommand = (filenames) =>
-  `yarn --cwd ./functions eslint ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' ')}`;
+  `eslint ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
 
 const buildFormatCommand = (filenames) =>
-  `yarn --cwd ./functions prettier ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' ')}`;
+  `prettier ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
 
 module.exports = {
   '**/*.{js,jsx,ts,tsx}': [buildLintCommand],
